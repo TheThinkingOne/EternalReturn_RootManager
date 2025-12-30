181c265256d12c79dbfdc3974857c13f6eff447a
TheThinkingOne <ousterss@gmail.com>
2025-12-30 15:18:36 +0900

    1230 uploading settings and image asset files

---
+++ src/api/apiPath.jsx
@@ -0,0 +1,9 @@
+export const API_SERVER_HOST = "http://localhost:8080";
+
+// 밑에 주소 바꿔야지
+export const PATHS = {
+  MEMBER: `${API_SERVER_HOST}/makemyday/member`,
+  TODO: `${API_SERVER_HOST}/makemyday/todo`,
+  QUOTES: `${API_SERVER_HOST}/makemyday/quotes`,
+  WALLPAPER: `${API_SERVER_HOST}/makemyday/wallpapers`,
+};
